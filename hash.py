1f4b81c75c731a5bf69b49f09e2ecddfba5279b6
HN <hni1124@naver.com>
2018-11-14 12:19:24 +0900

    modified the album function

--- hash.py
+++ hash.py
@@ -104,36 +104,44 @@ def spy_combine2(clothes):
 
 
 class Album:
-    chain = dict()
-
     def __init__(self, play, idx):
+        self.chain = dict()
         self.chain[play] = idx
 
     def __insert__(self, play, idx):
-        if play not in self.chain:
-            self.chain[play] = idx
+        self.chain[play] = idx if play not in self.chain else None
 
     def __sum__(self):
-        total = 0
-        for i in self.chain:
-            total += i
-        return total
+        return sum(i for i in self.chain)
 
     def __max__(self):
+        res = list()
         _max = 0
+        _sec = 0
         for i in self.chain:
             if _max < i:
+                _sec = _max
                 _max = i
-        return self.chain[_max]
+            elif _sec < i:
+                _sec = i
+        res.append(self.chain[_max])
+        if _sec > 0:
+            res.append(self.chain[_sec])
+        return res
 
 
 def best_album():
-    genres = ["classic", "pop", "classic", "classic", "pop"]
-    plays = [500, 600, 150, 800, 2500]
+    # genres = ["classic", "pop", "classic", "classic", "pop"]
+    # plays = [500, 600, 150, 800, 2500]
+    # genres = ["classic", "pop", "classic", "classic", "pop", "rock"]
+    # plays = [500, 600, 150, 800, 2500, 1800]
+    genres = ["rock", "rock", "rock", "rock", "rock", "rock"]
+    plays = [500, 600, 150, 800, 2500, 180]
 
     # 1. 장르별 play수가 가장 많은 장르부터 수록
     # 2. play수가 가장 많은 장르 내에 가장 많이 재생된 노래 먼저 수록
     # 3. 장르내 재생 횟수가 같은 노래중 고유번호가 낮은 노래부터 수록
+    # 4. 한 장르에 값이 한개만 있는 경우, 두 장르의 total이 같을 경우, 장르 자체가 한개만 있을 경우
 
     unique = dict()
     unique_list = list()
@@ -144,9 +152,26 @@ def best_album():
             unique[val] = Album(plays[idx], idx)
             unique_list.append(val)
 
+    max = 0
+    max_idx = ""
+    sec = 0
+    sec_idx = ""
+
     for i in unique_list:
-        print(unique[i])
-    print(unique_list)
+        if max < unique[i].__sum__():
+            sec = max
+            sec_idx = max_idx
+            max = unique[i].__sum__()
+            max_idx = i
+        elif sec < unique[i].__sum__():
+            sec = unique[i].__sum__()
+            sec_idx = i
+
+    res = unique[max_idx].__max__()
+    if sec > 0:
+        res += unique[sec_idx].__max__()
+
+    print(res)
 
 
 
